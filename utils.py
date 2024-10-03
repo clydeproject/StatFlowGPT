7a28e6138be0758f82e9f8c0dd16960c507a6151
Paartha <paarthanimbalkar@gmail.com>
2024-10-04 02:00:35 +0530

    added functionality to Analyst to use different LLMs for different stages of the pipeline

--- utils.py
+++ utils.py
@@ -27,11 +27,34 @@ class SimpleCodeExecutor:
         return {"success": True, "output": self.stdout_buffer.getvalue(), "error": self.stderr_buffer.getvalue()}
 
 class Analyst:
-    def __init__(self,df,client,model=None,code_executor=SimpleCodeExecutor):
+    '''
+    df: pd.DataFrame
+    client: LiteLLMWrapper|TogetherAIWrapper|OpenAIWrapper
+    model: str
+    code_executor: SimpleCodeExecutor
+    model_stack: {"rephrase_query":"model1","code_generation":"model2","error_correction":"model3"} | Use different models for each aspect of the pipeline 
+    
+    '''
+    def __init__(self,df,client,model=None,code_executor=SimpleCodeExecutor,model_stack=None):
         self.df = df 
         self.client = client 
         self.model = model 
         self.code_executor = code_executor(environment="st",df=self.df)#run dynamically generated code locally
+        self.model_stack = model_stack
+        #default chain run with a single model
+        if model_stack is None:
+            self.model_stack = {
+                "rephrase_query":self.model,
+                "code_generation":self.model,
+                "error_correction":self.model,
+                }
+        else:
+            self.model_stack = {
+                "rephrase_query":model_stack["rephrase_query"],
+                "code_generation":model_stack["code_generation"],
+                "error_correction":model_stack["error_correction"],
+                }      
+    
 
     def rephrase_query(self,query,model):
         prompt = REPHRASE_PROMPT.format(
@@ -40,9 +63,10 @@ class Analyst:
             userquery = query,
             )
         
-        rephrased_query = self.client.completion(
+        rephrased_query = self.client.chat_completion(
             model=model,temperature=0,max_tokens=256,
             messages=[{"role":"user","content":prompt},],
+            stream=False,
             )
         return rephrased_query
     
@@ -57,7 +81,8 @@ class Analyst:
 
         code = self.client.chat_completion(
             model=model,temperature=0,max_tokens=1000,
-            messages = [{"role":"user","content":prompt}]
+            messages = [{"role":"user","content":prompt}],
+            stream=False,
         )
         return code 
 
@@ -74,7 +99,8 @@ class Analyst:
 
         error_correction = self.client.chat_completion(
             model=model,temperature=0,max_tokens=1000,
-            messages= [{"role":"user","content":prompt}]
+            messages= [{"role":"user","content":prompt}],
+            stream=False,
         )
         return error_correction
 
@@ -96,13 +122,13 @@ class Analyst:
         return '\n'.join(python_code_blocks) if python_code_blocks else generation
 
     def run_chain(self,query):
-        if self.model is None:
-            raise ValueError(f"To use run_chain() an LLM model has to be chosen, please choose a model while instantiating Analyst()")
+        if self.model and self.model_stack is None:
+            raise ValueError(f"To use run_chain() an LLM model/model_stack has to be chosen, please choose a model/model_stack while instantiating Analyst()")
         
         #query answering chain 
-        rephrased_query = self.rephrase_query(query,model=self.model)
+        rephrased_query = self.rephrase_query(query,model=self.model_stack["rephrase_query"])
         print(rephrased_query)#debug
-        generated_code = self.generate_code(query=rephrased_query,model=self.model)
+        generated_code = self.generate_code(query=rephrased_query,model=self.model_stack["code_generation"])
         print(generated_code)#debug
         #state of the execution of generated code is the following format for SimpleCode Executor
         # {'success': Bool, 'output': Str, 'error': Bool}
@@ -116,7 +142,7 @@ class Analyst:
                 query,
                 generated_code,
                 state["error"],
-                model=self.model
+                model=self.model_stack["error_correction"]
                 )
             print(corrected_code)#debug
             state = self.code_executor.run(self.extract_code_from_generation(corrected_code))
