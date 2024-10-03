7abbeb7031ef2cf6a74aa0b81c1472389d06d887
Paartha <paarthanimbalkar@gmail.com>
2024-10-04 01:58:01 +0530

    Added support to LiteLLM(access 100+ LLMs)

--- wrappers.py
+++ wrappers.py
@@ -1,7 +1,21 @@
 import os
-import openai 
+import litellm
 from dotenv import load_dotenv
+from openai import OpenAI
 
+
+class LiteLLMWrapper:
+    '''
+    Wrapper around liteLLM, gives you access to multiple LLMs (100+ LLMs) 
+    '''
+    def __init__(self,):
+        load_dotenv()
+    def chat_completion(self,**kwargs):
+        response = litellm.completion(**kwargs)
+        if kwargs["stream"]:
+            return response
+        return response.choices[0].message.content
+    
 class OpenAIWrapper:
     '''
     model:int
@@ -15,22 +29,20 @@ class OpenAIWrapper:
         load_dotenv()
         self.__api_key = os.getenv("OPENAI_API_KEY")
         self.__api_base = "https://api.openai.com/v1/"
-        #initialize key and base 
-        openai.api_key = self.__api_key
-        openai.api_base = self.__api_base
+        self.client = OpenAI(api_key=self.__api_key,base_url=self.__api_base)
 
     def chat_completion(self,**kwargs):
-        response = openai.ChatCompletion.create(**kwargs)
+        response = self.client.chat.completions.create(**kwargs)
         if kwargs.get("stream", False):
             return response#generator object 
-        return response["choices"][0]["message"]["content"]
+        return response.choices[0].message.content
 
     def completion(self,**kwargs):
-        response = openai.Completion.create(**kwargs)
+        response = self.client.completions.create(**kwargs)
         if kwargs.get("stream", False):
             return response# generator object 
-        return response["choices"][0]["text"]
-    
+        return response.choices[0].text
+
 class TogetherAPIWrapper(OpenAIWrapper):
     '''
     model:int
@@ -45,7 +57,4 @@ class TogetherAPIWrapper(OpenAIWrapper):
         super(TogetherAPIWrapper).__init__()
         self.__api_key = os.getenv("TOGETHER_API_KEY")
         self.__api_base = "https://api.together.xyz/v1"
-        openai.api_key = self.__api_key
-        openai.api_base = self.__api_base
-
-
+        self.client = OpenAI(api_key=self.__api_key,base_url=self.__api_base)
